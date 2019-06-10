06558e6fb519d587608983e882408f022693f908
Meng Ye <yem3@uci.edu>
2019-06-10 11:35:19 -0700

    teacher_unavailability added

--- src/components/Constants/routes.js
+++ src/components/Constants/routes.js
@@ -4,6 +4,7 @@ export const SIGN_IN = '/signin';
 export const HOME = '/home';
 export const ptf = '/programTemplateHome';
 export const ttf = '/teacherTemplateHome';
+export const ttu = '/teacherTemplateUnavailability'
 export const ctf = '/classTemplateHome';
 export const ptfc = '/programTemplateForm';
 export const ttfc = '/teacherTemplateForm';
